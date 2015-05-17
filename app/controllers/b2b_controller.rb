dcc81ba54f8ee16379a752bc743a95d1bedc3f80
Damir Mandakovic <damirmandakovic@gmail.com>
2015-05-17 15:13:27 -0400

    Estructura preliminar de Notificar orden aceptada

--- app/controllers/b2b_controller.rb
+++ app/controllers/b2b_controller.rb
@@ -10,6 +10,19 @@ class B2bController < ApplicationController
 	end
 
 	def notify_accepted_order
+    # Este método debe ir acompañado por un parámetro que es un string del id de la orden de compra.
+    # Este parámetro se pasará más abajo como id de orden de compra.
+
+    # Primero hay que verificar Token. Si no existe, retornar error 401
+    require 'net/http'
+    result = Net::HTTP.get(URI.parse('http://chiri.ing.puc.cl/atenea/obtener/id'))
+
+    if result == error then
+      # retornar error
+    else
+      # retornar que está bien. Hay que ver si esto debiera ir acompañado de un pago.
+    end
+
 		
 	end
 
