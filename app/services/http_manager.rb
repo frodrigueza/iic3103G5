3f0a8face16e71004560edec720053ca5a53bb1a
Matias Vergara <mvergarair@gmail.com>
2015-06-01 17:35:52 -0300

    http manager

--- app/services/http_manager.rb
+++ app/services/http_manager.rb
@@ -1,8 +1,174 @@
 class HttpManager
-	def self.get_order(order_id)
-		uri = URI.parse('http://chiri.ing.puc.cl/atenea/obtener/' + order_id.to_s)
-	    response = Net::HTTP.get_response(uri)
+
+	uri = 'http://chiri.ing.puc.cl:8080/grupo5/webresources/'
+
+
+	def self.crear_oc(body)
+
+		url = uri +'atenea/crear' 
+
+		response = HTTParty.put(url, 
+			:body => body.to_json,
+    		:headers => { 'Content-Type' => 'application/json'})
+
+		order_hash = JSON.parse(response.body)[0]
+	
+	end
+
+	def self.recepcionar_oc(id_oc)
+
+		url = uri + 'atenea/recepcionar/' + id_oc.to_s
+
+		response = HTTParty.post(url , :body => {:id=>id }.to_json , :headers =>{ 'Content-Type' => 'application/json'})
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	def self.rechazar_oc(body)
+
+
+		url = uri + 'atenea/rechazar/' + body[:id_oc].to_s
+
+		response = HTTParty.post(url , :body => {:motivo => body[:motivo]}.to_json , :headers =>{ 'Content-Type' => 'application/json'})
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	def self.anular_oc(body)
+
+		url = uri + 'atenea/anular/' + body[:id_oc].to_s
+
+		response = HTTParty.delete(url, :body => { :motivo => body[:motivo]}.to_json , :headers =>{ 'Content-Type' => 'application/json'})
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	def self.get_oc(id_oc)
+
+		url = uri + 'atenea/obtener/' + id_oc.to_s
+
+	    response = HTTParty.get(url)
 
 	    order_hash = JSON.parse(response.body)[0]
+
 	end
+
+
+	def self.despachar(id_oc)
+
+		url = uri + 'atenea/despachar/' + id_oc.to_s
+
+		response = HTTParty.post(url , :body => {}.to_json , :headers =>{ 'Content-Type' => 'application/json'})
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	
+	def self.emitir_factura(id_oc)
+
+		url = uri + 'zeuz/' 
+
+		response = HTTParty.put(url , :body => {:oc => body[:id_oc]}.to_json , :headers =>{ 'Content-Type' => 'application/json'})
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+
+	def self.obtener_factura(id_f)
+
+		url = uri + 'zeuz/' + id_f.to_s
+
+		response = HTTParty.get(url)
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	def self.pagar_factura(id_f)
+
+		url = uri + 'zeuz/pay/'
+
+		response = HTTParty.post(url , :body => {:id => body[:id_f]}.to_json , :headers =>{ 'Content-Type' => 'application/json'})
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	def self.rechazar_factura(body)
+
+		url = uri + 'zeuz/reject/'
+
+		response = HTTParty.post(url , :body => {:id => body[:id_f] , :motivo => body[:motivo]}.to_json , :headers =>{ 'Content-Type' => 'application/json'})
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	def self.anular_factura(body)
+
+		url = uri + 'zeuz/cancel/'
+
+		response = HTTParty.post(url , :body => {:id => body[:id_f] , :motivo => body[:motivo]}.to_json , :headers =>{ 'Content-Type' => 'application/json'})
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	# def self.transferir(body)
+
+	# 	url = uri + 'apolo/trx/'
+
+	# 	response = HTTParty.put(url , 
+	# 		:query => {:monto => body[:monto] , :origen => body[:origen], :destino => body[:destino]}.to_json ,
+	# 		:body  => {}.to_json,
+	# 		:headers =>{ 'Content-Type' => 'application/json'})
+
+	# 	order_hash = JSON.parse(response.body)[0]
+
+	# end
+
+	def self.obtener_transaccion(id_t)
+
+		url = uri + 'apolo/trx/' + id_t.to_s
+
+		response = HTTParty.get(url)
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	# def self.obtener_cartola(body)
+
+	# 	url = uri + 'apolo/cartola/'
+
+	# 	response = HTTParty.post(url , 
+	# 		:query => {:fechaInicio => body[:fecha_inicio] , :fechaFin => body[:fecha_fin], :id => body[:id_cb], :limit => body[:limit]}.to_json ,
+	# 		:body  => {}.to_json,
+	# 		:headers =>{ 'Content-Type' => 'application/json'})
+
+	# 	order_hash = JSON.parse(response.body)[0]
+
+	# end
+
+	def self.obtener_cartola(id_cb)
+
+		url = uri + 'apolo/banco/cuenta/' + id_cb.to_s
+
+		response = HTTParty.get(url)
+
+		order_hash = JSON.parse(response.body)[0]
+
+	end
+
+	def self.test
+		puts "hola"
+		hash = get_oc('5563933be0949e0300344fda')
+		puts hash
+	end
+
 end
