738a12c22735fc86945305cedb5dc6a05b543267
jpterraz <jpterraz@uc.cl>
2015-06-01 18:01:49 -0300

    Cambio en bodega manager y factura manager

--- app/services/factura_manager.rb
+++ app/services/factura_manager.rb
@@ -2,11 +2,28 @@ class FacturaManager
 
   def self.emitir_factura(oc)
     #crear factura
+    factura = HttpManager.emitirFactura(oc)
 
+    return factura
     #notificar factura a grupo
   end
 
   def self.recibir_factura(id_factura)
+
+    facturaRecibida = HttpManager.obtenerFactura(id_factura)
+
+    if facturaRecibida.cliente!='5'
+      error = HttpManager.rechazarFactura(id_factura, 'no somos nosotros')
+      return error 
+    #Rechazar en el futuro si nos falta $$$
+    else
+
+      pagado = HttpManager.pagarFactura(id_factura)   
+
+      PedidoManager.check_ready
+
+      return pagado
+
     #Criterio para aceptar o rechazar factura
     # Si acepta factura
       # Setear factura aceptada en API del curso
