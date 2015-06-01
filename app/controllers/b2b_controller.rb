57144d059abb3014111a1b1f913d0af861366948
guillermo <g_ibacache@hotmail.com>
2015-06-01 19:29:31 -0300

    cambios en documentacion y errores de flujo

--- app/controllers/b2b_controller.rb
+++ app/controllers/b2b_controller.rb
@@ -14,78 +14,14 @@ class B2bController < ApplicationController
 
 	# API para otros grupos
 	def new_order
-		order_id = params[:order_id]
-
-		p "orden recepcionada ----------------------------------------------- "
-		p order_id
-
-		uri = URI.parse('http://chiri.ing.puc.cl/atenea/obtener/' + order_id.to_s)
-	    response = Net::HTTP.get_response(uri)
-
-	    order_hash = JSON.parse(response.body)[0]
-	    id_oc = order_hash["_id"]
-	    cliente = order_hash["cliente"]
-
-	    #Lista de codigos que poseemos. Si no esta 
-	    skus = [5,26,27,28,29,30,44]
-
-	    accepted = false
-
-	    if id_oc != nil 
-	    	p "paso nil   "
-	    	p order_hash["proveedor"]
-	    	p order_hash["canal"]
-	    	p order_hash["sku"]
-	    	if order_hash["proveedor"] == "5" and order_hash["canal"] == "b2b" and skus.include? order_hash["sku"].to_i
-	    		p "paso if "
-
-	    			accepted = true
-
-	    			#Recepcionar OC
-	    			uri = URI.parse('http://chiri.ing.puc.cl/atenea/recepcionar/' + order_id.to_s)
-	    			query = {order_id: id_oc}
-					response = Net::HTTP.post_form(uri, query)
-
-					#Revisar stock de materia prima en el almacen libre dispocicion
-					id_almacen = "554be8d4b6355d03001ff96c"
-					hash = "hY8ytD5TLHvU8wVMUlRS8KQ4jHA="
-					header = {'Authorization' => "INTEGRACION grupo5:"+ hash}
-					query = {almacenId: id_almacen}
-					uri = URI.parse('http://integracion-2015-dev.herokuapp.com/bodega/skusWithStock/')
-					response = HTTParty.get(uri,:query => query,:headers => header)
-					# response.each do |product|
-					# 	if product[:id] == order_hash["sku"]
-					# 		if product
-								
-					# 		end
-					# 	end
-					# end
-
-					#Ver si el stock es suficiente
-
-					#mover stock a bodega de despacho
-
-					#despachar
-
-
-					#Emitir factura
-					@id_factura = "Emitir factura sin URL en la api del curso"
-
-			end
+		answer = OrdersManager.manage_order params[:order_id]
+		respond_to do |format|
+			format.json {render json: answer[:content], status: answer[:status]}
 		end
-					
 
-	    respond_to do |format|
-    		if accepted
-    			format.json {render json: {id_oc: id_oc, id_factura: @id_factura}, status: 200}
-    			#format.json {render json: {id_oc: id_oc, id_factura: @id_factura}, status: 200}
-    		else
-    		 	format.json {render json: {msg: 'Orden de compra no valida'}, status: 400}
-    		end
-	    end
 	end
 
-	def notify_order_accepted
+	def order_accepted
 	    order_id = params[:order_id]
 
 	    uri = URI.parse('http://chiri.ing.puc.cl/atenea/obtener/' + order_id.to_s)
@@ -102,7 +38,7 @@ class B2bController < ApplicationController
 	    end
   	end
 
-	def notify_order_rejected
+	def order_rejected
 		order_id = params[:order_id]
 
 	    uri = URI.parse('http://chiri.ing.puc.cl/atenea/obtener/' + order_id.to_s)
@@ -119,7 +55,7 @@ class B2bController < ApplicationController
 	    end
 	end
 
-	def notify_order_canceled
+	def order_canceled
 		order_id = params[:order_id]
 
 	    uri = URI.parse('http://chiri.ing.puc.cl/atenea/obtener/' + order_id.to_s)
