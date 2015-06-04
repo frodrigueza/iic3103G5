5dac1d8adf19f005dd0eb38f6f32579ecdcc245c
frodrigueza <feliperodriguezarteaga@gmail.com>
2015-06-04 12:48:10 -0400

    correcion api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	if params[:order_id] && params[:order_id] != ""
   		oc_id = params[:order_id]
-  		if @oc = HttpManager.get_oc(oc_id)
+  		if @oc = HttpManager.exist_order(oc_id)
   			if @oc[:_id]
   				@oc = OpenStruct.new(@oc).to_h
   				@pedido = Pedido.find_by(oc_id: oc_id)
